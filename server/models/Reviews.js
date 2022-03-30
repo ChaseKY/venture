4f621a011b44bb93f8bcc6a1d16660ad3f1152d7
Chase Young <chasekyoung@gmail.com>
2022-03-30 14:00:41 -0400

    created Reviews and SavedHikes models

---
+++ server/models/Reviews.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Reviews extends Model {}
+
+Reviews.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    trailId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    user: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'User',
+        key: 'username',
+      },
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    review: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Reviews',
+  }
+);
