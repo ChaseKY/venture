667cb856b749635b3c657fa6912ea57816e34ca4
Chase Young <chasekyoung@gmail.com>
2022-04-01 11:48:19 -0400

    deleted client contents and added original contents

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
 const reviewRoutes = require('./reviewRoutes');
+const savedHikeRoutes = require('./savedHikeRoutes');
 
 router.use('/user', userRoutes);
 router.use('/review', reviewRoutes);
+router.use('/savedHike', savedHikeRoutes);
 
 module.exports = router;
